6db97f42b6800aefd500b79bb77a1e6b419784a9
danyalll <ne.danyalll@gmail.com>
2023-02-20 14:33:20 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -11,3 +11,27 @@ const swiper = new Swiper('.swiper', {
             prevEl: '.swiper-button-left',
       },
 });
+
+
+ymaps.ready(init);
+function init(){
+      // Создание карты.
+      var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [59.891910, 30.442121],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 15
+      });
+}
+
+
+const menuBtn = document.querySelector('.menu-btn')
+const menuMob = document.querySelector('.menu-mobile')
+menuBtn.addEventListener('click', () => {
+      menuMob.classList.toggle('menu--open')
+      menuBtn.classList.toggle('menu--open')
+})
